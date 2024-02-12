a7ec26876c6151caada9e8818beb46210c5ec22e
izanami <120745634+IzanamiiDevv@users.noreply.github.com>
2024-02-12 23:49:05 +0800

    Add files via upload

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace ASP.NET_Core_with_React.js
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
